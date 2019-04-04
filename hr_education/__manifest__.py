9ad88b773b0ab822eb38a6280f448481bdc98335
oihane <oihanecruce@gmail.com>
2019-04-04 17:28:43 +0200

    [ADD] New module hr_education (#6)

---
+++ hr_education/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2019 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Human Resources for Education",
+    "version": "12.0.1.0.0",
+    "category": "Human Resources",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "hr",
+        "education",
+    ],
+    "data": [
+        "views/hr_employee_view.xml",
+    ],
+    "installable": True,
+}
---
+++ hr_education/models/hr_employee.py
@@ -0,0 +1,18 @@
+# Copyright 2019 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class HrEmployee(models.Model):
+    _inherit = 'hr.employee'
+
+    edu_language_ids = fields.Many2many(
+        comodel_name='education.language', relation='edu_employee_language',
+        column1='employee_id', column2='edu_language_id', string='Languages')
+    edu_level_ids = fields.Many2many(
+        comodel_name='education.level', relation='edu_employee_level',
+        column1='employee_id', column2='edu_level_id', string='Levels')
+    edu_subject_ids = fields.Many2many(
+        comodel_name='education.subject', relation='edu_employee_subject',
+        column1='employee_id', column2='edu_subject_id', string='Subjects')
