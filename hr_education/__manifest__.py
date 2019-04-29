11d5eb771e8c535981c30d16f0eb93281b3b8d90
oihane <oihanecruce@gmail.com>
2019-04-29 10:03:16 +0200

    [IMP] hr_education: add course, level and field to subjects (#8)
    * [IMP] hr_education: add course, level and field to subjects
    
    * [IMP] hr_education: add tests

--- hr_education/__manifest__.py
+++ hr_education/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Human Resources for Education",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.1.0",
     "category": "Human Resources",
     "license": "AGPL-3",
     "author": "AvanzOSC",
