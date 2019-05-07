32b83b4063d0575609c8b9739106fe35aaae9822
Oihane Crucelaegui <oihanecruce@gmail.com>
2019-05-07 18:09:49 +0200

    [IMP] education: center required

--- education/models/education_classroom.py
+++ education/models/education_classroom.py
@@ -1,7 +1,7 @@
 # Copyright 2019 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class EducationClassroom(models.Model):
@@ -12,4 +12,21 @@ class EducationClassroom(models.Model):
     capacity = fields.Integer(string='Seating Capacity')
     center_id = fields.Many2one(
         comodel_name='res.partner', string='Education Center',
-        domain=[('education_code', '!=', False)])
+        required=True)
+
+    @api.multi
+    def name_get(self):
+        """ name_get() -> [(id, name), ...]
+
+        Returns a textual representation for the records in ``self``.
+        By default this is the value of the ``display_name`` field.
+
+        :return: list of pairs ``(id, text_repr)`` for each records
+        :rtype: list(tuple)
+        """
+        result = []
+        for record in self:
+            result.append((record.id, '[{}] {} ({})'.format(
+                record.education_code, record.description,
+                record.center_id.name)))
+        return result
