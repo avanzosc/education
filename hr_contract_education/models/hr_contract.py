6e6e8d9bffb2aadf4c230cea6dde882b32c1480c
Oihane Crucelaegui <oihanecruce@gmail.com>
2019-05-21 15:19:44 +0200

    [IMP] hr_contract_education: add fields from hezkuntza

--- hr_contract_education/models/hr_contract.py
+++ hr_contract_education/models/hr_contract.py
@@ -16,10 +16,22 @@ class HrContract(models.Model):
     ed_otherposition_id = fields.Many2one(
         comodel_name='education.position', string='Other Position',
         domain=[('type', '=', 'other')])
+    ed_designation_id = fields.Many2one(
+        comodel_name='education.designation_level', string='Designation Level')
     ed_work_reason_id = fields.Many2one(
         comodel_name='education.work_reason', string='Work Reason')
     ed_contract_type_id = fields.Many2one(
         comodel_name='education.contract_type', string='Contract Type')
     ed_workday_type_id = fields.Many2one(
         comodel_name='education.workday_type', string='Workday Type')
+    ed_work_hours = fields.Float(string='Total Hours')
+    ed_class_hours = fields.Float(string='Class Hours')
     ed_contract_hours = fields.Float(string='Contract Hours')
+    ed_center_id = fields.Many2one(
+        comodel_name='res.partner', string='Education Center')
+    ed_academic_year_id = fields.Many2one(
+        comodel_name='education.academic_year', string='Academic Year')
+    ed_reduction_health = fields.Boolean(
+        string='Reduction for health reasons')
+    ed_reduction_age = fields.Boolean(
+        string='Reduction for being over 60 years old')
