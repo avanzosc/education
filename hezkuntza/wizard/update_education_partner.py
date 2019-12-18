28330ef8308abf42c9913727d8ed43703e881ae2
Oihane Crucelaegui <oihanecruce@gmail.com>
2019-12-18 17:06:18 +0100

    [IMP] hezkuntza: search more accurate

--- hezkuntza/wizard/update_education_partner.py
+++ hezkuntza/wizard/update_education_partner.py
@@ -27,15 +27,15 @@ class UpdateEducationResPartner(models.TransientModel):
         string="Lines")
     partner_line_ids = fields.One2many(
         comodel_name="update.education.partner.line", inverse_name="wizard_id",
-        string="Partner Lines", domain=[('partner_id', '!=', False)])
+        string="Partner Lines", domain=[("partner_id", "!=", False)])
     missing_partner_line_ids = fields.One2many(
         comodel_name="update.education.partner.line", inverse_name="wizard_id",
         string="Missing Partner Lines",
-        domain=[('missing_partner', '=', True)])
+        domain=[("missing_partner", "=", True)])
     duplicated_partner_line_ids = fields.One2many(
         comodel_name="update.education.partner.line", inverse_name="wizard_id",
         string="Duplicated Partner Lines",
-        domain=[('duplicated_partner', '=', True)])
+        domain=[("duplicated_partner", "=", True)])
     partner_count = fields.Integer(
         compute="_compute_partner_count", store=True)
     missing_partner_count = fields.Integer(
@@ -47,11 +47,11 @@ class UpdateEducationResPartner(models.TransientModel):
     def _compute_partner_count(self):
         for wizard in self:
             wizard.partner_count = len(
-                wizard.file_line_ids.filtered('partner_id'))
+                wizard.file_line_ids.filtered("partner_id"))
             wizard.missing_partner_count = len(
-                wizard.file_line_ids.filtered('missing_partner'))
+                wizard.file_line_ids.filtered("missing_partner"))
             wizard.duplicated_partner_count = len(
-                wizard.file_line_ids.filtered('duplicated_partner'))
+                wizard.file_line_ids.filtered("duplicated_partner"))
 
     @api.multi
     def upload_file(self):
@@ -81,22 +81,22 @@ class UpdateEducationResPartner(models.TransientModel):
 
     @api.multi
     def button_update_education_code(self):
-        self.mapped('file_line_ids').button_update_education_code()
+        self.mapped("file_line_ids").button_update_education_code()
 
     @api.multi
     def button_update_vat(self):
-        self.mapped('file_line_ids').button_update_vat()
+        self.mapped("file_line_ids").button_update_vat()
 
     @api.multi
     def button_update_education_code_and_vat(self):
-        self.mapped('file_line_ids').button_update_education_code_and_vat()
+        self.mapped("file_line_ids").button_update_education_code_and_vat()
 
 
 class UpdateEducationResPartnerLine(models.TransientModel):
     _name = "update.education.partner.line"
     _description = "Wizard Lines to Update Partner Education Code"
     _order = "student_lastname1, student_lastname2, student_name"
-    
+
     wizard_id = fields.Many2one(
         comodel_name="update.education.partner", string="Wizard",
         required=True)
@@ -117,7 +117,7 @@ class UpdateEducationResPartnerLine(models.TransientModel):
         related="partner_id.education_code", string="Partner Education Code")
     vat = fields.Char(
         related="partner_id.vat", string="Partner Tax ID")
-    
+
     @api.depends("student_document", "student_lastname1", "student_lastname2",
                  "student_name", "student_education_code")
     def _compute_partner(self):
@@ -135,15 +135,15 @@ class UpdateEducationResPartnerLine(models.TransientModel):
 
     def _find_partner_ids(self):
         self.ensure_one()
-        partners = partner_obj = self.env['res.partner']
+        partners = partner_obj = self.env["res.partner"]
         if self.student_document:
             partners = partner_obj.search([
                 ("vat", "ilike", self.student_document)])
         if not partners:
             partners = partner_obj.search([
-                ('lastname', '=', self.student_lastname1),
-                ('lastname2', '=', self.student_lastname2),
-                ('firstname', '=', self.student_name),
+                ("lastname", "=ilike", self.student_lastname1.encode("utf-8")),
+                ("lastname2", "=ilike", self.student_lastname2.encode("utf-8")),
+                ("firstname", "=ilike", self.student_name.encode("utf-8")),
             ])
         return partners
 
@@ -161,19 +161,17 @@ class UpdateEducationResPartnerLine(models.TransientModel):
 
     @api.multi
     def button_update_education_code(self):
-        for line in self.filtered('partner_id'):
+        for line in self.filtered("partner_id"):
             line.partner_id.education_code = line.student_education_code
-        return True
 
     @api.multi
     def button_update_vat(self):
         for line in self.filtered(
                 lambda l: l.partner_id and l.student_document):
             try:
-                line.partner_id.vat = line.student_document
+                line.partner_id.vat = "ES{}".format(line.student_document)
             except Exception:
                 pass
-        return True
 
     @api.multi
     def button_update_education_code_and_vat(self):
