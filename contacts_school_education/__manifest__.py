36f843666811cbc6b7009c28c72aca4bd6ab6080
oihane <oihanecruce@gmail.com>
2020-02-12 14:30:21 +0100

    [IMP] contacts_school_education: course change must be unique + search view (#73)

--- contacts_school_education/__manifest__.py
+++ contacts_school_education/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "School Education",
+    "name": "Contacts for Education Centers",
     "version": "12.0.1.0.0",
     "category": "Tools",
     "license": "AGPL-3",
