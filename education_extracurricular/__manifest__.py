d1cbc6723f740d980673bc141f6c436ddc96a555
Tu Nombre <you@example.com>
2020-05-04 17:44:47 +0200

    [ADD] education_extracurricular

---
+++ education_extracurricular/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2020 Daniel Campos - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Education Extracurricular",
+    "version": "12.0.1.0.0",
+    "category": "Education",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "education",
+        "product",
+    ],
+    "data": [
+        "views/product_view.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
