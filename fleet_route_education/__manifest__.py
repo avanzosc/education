08ce0cdc359cb96b3739af05025b05bff9ca036a
Oihane Crucelaegui <oihanecruce@gmail.com>
2020-06-09 15:27:54 +0200

    [ADD] new module fleet_route_education

---
+++ fleet_route_education/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2019 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Fleet Route Education",
+    "version": "12.0.1.0.0",
+    "category": "Human Resources",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "fleet_route_school",
+        "contacts_school_education",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "reports/res_partner_fleet_route_report_view.xml",
+        "views/fleet_route_stop_view.xml",
+    ],
+    "installable": True,
+}
