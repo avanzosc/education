5bd7943587465376e0540268bcd9b477a46657eb
oihane <oihanecruce@gmail.com>
2020-06-17 13:59:13 +0200

    [IMP] contacts_school_education: create schedule from group (#121)

--- contacts_school_education/tests/test_contacts_school_education.py
+++ contacts_school_education/tests/test_contacts_school_education.py
@@ -3,7 +3,7 @@
 
 from .common import TestContactsSchoolEducationCommon
 from odoo.tests import common
-from odoo.exceptions import ValidationError
+from odoo.exceptions import UserError, ValidationError
 
 
 @common.at_install(False)
@@ -15,10 +15,10 @@ class TestContactsSchoolEducation(TestContactsSchoolEducationCommon):
         self.assertEquals(self.student.current_group_id, self.group)
         self.assertEquals(self.family, self.student.parent_id)
         students = self.partner_model.search([
-            ('current_center_id', '=', self.school.id)])
+            ('current_center_id', '=', self.edu_partner.id)])
         self.assertIn(self.student, students)
         students = self.partner_model.search([
-            ('current_center_id', 'ilike', self.school.name)])
+            ('current_center_id', 'ilike', self.edu_partner.name)])
         self.assertIn(self.student, students)
         students = self.partner_model.search([
             ('current_course_id', '=', self.edu_course.id)])
@@ -27,15 +27,15 @@ class TestContactsSchoolEducation(TestContactsSchoolEducationCommon):
             ('current_course_id', 'ilike', self.edu_course.description)])
         self.assertIn(self.student, students)
         parents = self.partner_model.search([
-            ('childs_current_center_ids', '=', self.school.id)])
+            ('childs_current_center_ids', '=', self.edu_partner.id)])
         self.assertIn(self.family, parents)
         parents = self.partner_model.search([
-            ('childs_current_center_ids', 'ilike', self.school.name)])
+            ('childs_current_center_ids', 'ilike', self.edu_partner.name)])
         self.assertIn(self.family, parents)
         parents = self.partner_model.search([
             ('childs_current_course_ids', '=', self.edu_course.id)])
         self.assertIn(self.family, parents)
-        self.assertIn(self.school, self.family.childs_current_center_ids)
+        self.assertIn(self.edu_partner, self.family.childs_current_center_ids)
         self.assertIn(self.edu_course, self.family.childs_current_course_ids)
         parents = self.partner_model.search([
             ('childs_current_course_ids', 'ilike',
@@ -43,27 +43,30 @@ class TestContactsSchoolEducation(TestContactsSchoolEducationCommon):
         self.assertIn(self.family, parents)
         action_dict = self.family.button_open_current_student()
         self.assertFalse(action_dict)
-        school_action_dict = self.school.button_open_current_student()
-        self.assertIn(("current_center_id", "=", self.school.id),
+        school_action_dict = self.edu_partner.button_open_current_student()
+        self.assertIn(("current_center_id", "=", self.edu_partner.id),
                       school_action_dict.get("domain"))
-        action_dict = self.school.button_open_relative_student()
+        action_dict = self.edu_partner.button_open_relative_student()
         self.assertFalse(action_dict)
         family_action_dict = self.family.button_open_relative_student()
         students = self.family.mapped("family_ids.child2_id")
         self.assertIn(("student_id", "in", students.ids),
                       family_action_dict.get("domain"))
+        self.student.educational_category = "otherchild"
+        with self.assertRaises(UserError):
+            self.student.get_current_group()
 
     def test_course_change(self):
         with self.assertRaises(ValidationError):
             self.change_model.create({
-                'school_id': self.school.id,
-                'next_school_id': self.school.id,
+                'school_id': self.edu_partner.id,
+                'next_school_id': self.edu_partner.id,
                 'course_id': self.edu_course.id,
                 'next_course_id': self.edu_course.id,
             })
         change_model = self.change_model.create({
-            'school_id': self.school.id,
-            'next_school_id': self.school.id,
+            'school_id': self.edu_partner.id,
+            'next_school_id': self.edu_partner.id,
             'course_id': self.edu_course2.id,
             'next_course_id': self.edu_course.id,
         })
@@ -78,22 +81,33 @@ class TestContactsSchoolEducation(TestContactsSchoolEducationCommon):
         self.assertIn(self.edu_subject, change_model.next_subject_ids)
         with self.assertRaises(ValidationError):
             self.change_model.create({
-                'school_id': self.school.id,
-                'next_school_id': self.school.id,
+                'school_id': self.edu_partner.id,
+                'next_school_id': self.edu_partner.id,
                 'course_id': self.edu_course2.id,
                 'next_course_id': self.edu_course.id,
             })
+        group2 = self.group.copy(
+            default={"academic_year_id": self.group.academic_year_id.id,
+                     "education_code": "NXTGROUP"})
+        self.assertFalse(group2.schedule_ids)
+        group2.create_schedule()
+        self.assertTrue(group2.schedule_ids)
+        self.assertEquals(
+            len(group2.schedule_ids), len(change_model.next_subject_ids))
 
     def test_school_classroom(self):
-        self.assertFalse(self.school.classroom_ids)
-        self.assertEquals(self.school.classroom_count, 0)
+        self.assertFalse(self.edu_partner2.classroom_ids)
+        self.assertEquals(self.edu_partner2.classroom_count, 0)
         new_classroom = self.classroom_model.create({
-            "center_id": self.school.id,
+            "center_id": self.edu_partner2.id,
             "education_code": "TEST",
             "description": "Test Classroom",
         })
-        self.assertIn(new_classroom, self.school.classroom_ids)
-        self.assertEquals(self.school.classroom_count, 1)
-        classroom_dict = self.school.button_open_classroom()
+        self.assertIn(new_classroom, self.edu_partner2.classroom_ids)
+        self.assertEquals(self.edu_partner2.classroom_count, 1)
+        classroom_dict = self.edu_partner2.button_open_classroom()
         self.assertIn(
-            ("center_id", "in", self.school.ids), classroom_dict.get('domain'))
+            ("center_id", "in", self.edu_partner2.ids),
+            classroom_dict.get('domain'))
+        classroom_dict = self.student.button_open_classroom()
+        self.assertFalse(classroom_dict)
