4e29f407db24a05e8d1d52e62bb2004e380236b8
oihane <oihanecruce@gmail.com>
2020-07-05 23:23:50 +0200

    [IMP] contacts_school_education: access subject list from course change (#135)

--- contacts_school_education/models/education_course_change.py
+++ contacts_school_education/models/education_course_change.py
@@ -3,6 +3,8 @@
 
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
+from odoo.models import expression
+from odoo.tools.safe_eval import safe_eval
 
 
 class EducationCourseChange(models.Model):
@@ -44,9 +46,30 @@ class EducationCourseChange(models.Model):
         comodel_name="education.plan", string="Next Education Plan",
         related="next_course_id.plan_id")
     next_subject_ids = fields.Many2many(
-        comodel_name="education.subject", string="Next Education Subjects")
+        comodel_name="education.subject", relation="subject_change_rel",
+        column1="course_change_id", column2="subject_id",
+        string="Next Education Subjects", compute="_compute_next_subject")
+    next_subject_count = fields.Integer(
+        string="# Subject", compute="_compute_next_subject")
     gender = fields.Selection(
         string="Gender", selection=_get_selection_gender, copy=False)
+
+    @api.multi
+    def _get_center_course_subject_list(self):
+        self.ensure_one()
+        return self.env["education.subject.center"].search([
+            ("center_id", "=", self.next_school_id.id),
+            ("level_id", "=", self.next_level_id.id),
+            ("course_id", "=", self.next_course_id.id),
+        ])
+
+    @api.depends("next_school_id", "next_level_id", "next_course_id")
+    def _compute_next_subject(self):
+        for record in self:
+            subject_centers = record._get_center_course_subject_list()
+            subjects = subject_centers.mapped("subject_id")
+            record.next_subject_ids = [(6, 0, subjects.ids)]
+            record.next_subject_count = len(subjects)
 
     @api.constrains("course_id", "next_course_id")
     def _check_different_course(self):
@@ -88,21 +111,21 @@ class EducationCourseChange(models.Model):
         return result
 
     @api.multi
-    def button_add_subject_list(self):
-        group_obj = self.env["education.group"]
-        for record in self.filtered(
-                lambda r: r.next_school_id and r.next_course_id):
-            edu_groups = group_obj.search([
-                ("academic_year_id.current", "=", True),
-                ("center_id", "=", record.next_school_id.id),
-                ("course_id", "=", record.next_course_id.id),
-                ("level_id", "=", record.next_course_id.level_id.id),
-            ])
-            subjects = edu_groups.mapped("schedule_ids.subject_id").filtered(
-                lambda s: s.level_ids == record.next_course_id.level_id and
-                s.course_ids == record.next_course_id)
-            if record.next_course_id.field_id:
-                subjects = subjects.filtered(
-                    lambda s: s.field_ids == record.next_course_id.field_id or
-                    not s.field_ids)
-            record.next_subject_ids = [(6, 0, subjects.ids)]
+    def button_open_subject_list(self):
+        self.ensure_one()
+        action = self.env.ref("education.action_education_subject_center")
+        action_dict = action and action.read()[0]
+        action_dict["context"] = safe_eval(
+            action_dict.get("context", "{}"))
+        action_dict['context'].update({
+            "default_center_id": self.next_school_id.id,
+            "default_level_id": self.next_level_id.id,
+            "default_course_id": self.next_course_id.id,
+        })
+        domain = expression.AND([
+            [("center_id", "=", self.next_school_id.id),
+             ("level_id", "=", self.next_level_id.id),
+             ("course_id", "=", self.next_course_id.id)],
+            safe_eval(action.domain or "[]")])
+        action_dict.update({"domain": domain})
+        return action_dict
