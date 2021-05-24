d9630121927d9efb3a7d82f36a473227a9f20a33
Oihane Crucelaegui <oihanecruce@gmail.com>
2021-05-24 17:39:58 +0200

    [FIX] hezkuntza: student code must be numeric

--- hezkuntza/models/education_course.py
+++ hezkuntza/models/education_course.py
@@ -1,12 +1,14 @@
 # Copyright 2019 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import _, api, models
+from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
 
 class EducationCourse(models.Model):
     _inherit = 'education.course'
+
+    course_value = fields.Integer(string="Course Value to Export XML")
 
     @api.constrains('education_code')
     def _check_education_code(self):
