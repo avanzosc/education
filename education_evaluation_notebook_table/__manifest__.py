77916fcaf279423f7e51d22c7122e7864399f069
Avanzosc <leiremartinez@avanzosc.es>
2021-08-24 12:58:37 +0200

    [ADD] education_evaluation_notebook_table: add website schedule records view

---
+++ education_evaluation_notebook_table/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2021 Leire Martinez de Santos - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Education Evaluation Notebook - Teacher Table",
+    "version": "12.0.1.0.0",
+    "category": "Hidden",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "portal",
+        "website",
+        "education_evaluation_notebook",
+    ],
+    "data": [
+        "views/templates.xml",
+        "views/views.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
---
+++ education_evaluation_notebook_table/controllers/controllers.py
@@ -0,0 +1,62 @@
+
+from odoo import http
+from odoo.http import request
+from odoo.addons.portal.controllers.portal import CustomerPortal
+
+
+class CoasMain(CustomerPortal):
+
+    @http.route(['/schedules'], type='http',
+                auth="user", website=True)
+    def schedule(self):
+
+        logged_employee = request.env['hr.employee'].search([
+            ('user_id', '=', request.uid)])
+
+        if not logged_employee:
+            return request.redirect('/main')
+
+        schedule_obj = request.env['education.schedule']
+        current_academic_year = self.get_current_academic_year()
+
+        schedules = schedule_obj.sudo().search([
+            ('academic_year_id', '=', current_academic_year.id),
+            ('teacher_id', '=', logged_employee.id)
+        ])
+
+        values = {
+            'schedules': schedules,
+        }
+
+        return http.request.render(
+            'education_evaluation_notebook_table.' +
+            'teacher_schedule_table', values)
+
+    @http.route(['/schedule/<int:schedule_id>/califications'], type='http',
+                auth="user", website=True)
+    def schedule_califications(self, schedule_id=None):
+
+        logged_employee = request.env['hr.employee'].search([
+            ('user_id', '=', request.uid)])
+
+        if not logged_employee:
+            return request.redirect('/main')
+
+        schedule_obj = request.env['education.schedule']
+
+        schedule = schedule_obj.sudo().browse(schedule_id)
+        schedule_evaluation_records = schedule.record_ids
+        evaluation_record_students = schedule_evaluation_records.mapped('student_id')
+
+        record_lines = schedule_evaluation_records.mapped('n_line_id')
+
+        values = {
+            'schedule': schedule,
+            'schedule_evaluation_records': schedule_evaluation_records,
+            'evaluation_record_students': evaluation_record_students,
+            'record_lines': record_lines,
+        }
+
+        return http.request.render(
+            'education_evaluation_notebook_table.' +
+            'schedule_calification_table', values)
