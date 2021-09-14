8920a053ed68bc63d790f36a11bd9c568b8d4e38
Oihane Crucelaegui <oihanecruce@gmail.com>
2021-09-14 13:54:32 +0200

    [IMP] education_issue: historic menues

--- education_evaluation_notebook_issue/models/school_claim.py
+++ education_evaluation_notebook_issue/models/school_claim.py
@@ -7,26 +7,18 @@ from odoo import api, fields, models
 class SchoolClaim(models.Model):
     _inherit = "school.claim"
 
-    academic_year_id = fields.Many2one(
-        comodel_name="education.academic_year", string="Academic Year",
-        compute="_compute_academic_year_evaluation", store=True)
     evaluation_id = fields.Many2one(
         comodel_name="education.academic_year.evaluation", string="Evaluation",
         compute="_compute_academic_year_evaluation", store=True)
 
-    @api.depends("issue_date", "school_id", "student_course_id")
+    @api.depends("academic_year_id", "school_id", "student_course_id")
     def _compute_academic_year_evaluation(self):
-        academic_year_obj = self.env["education.academic_year"]
         evaluation_obj = self.env["education.academic_year.evaluation"]
-        for issue in self:
-            issue.academic_year_id = academic_year_obj.search([
-                ("date_start", "<=", issue.issue_date),
-                ("date_end", ">=", issue.issue_date),
-            ], limit=1)
-            issue.evaluation_id = evaluation_obj.search([
-                ("academic_year_id", "=", issue.academic_year_id.id),
-                ("center_id", "=", issue.school_id.id),
-                ("course_id", "=", issue.student_course_id.id),
-                ("date_start", "<=", issue.issue_date),
-                ("date_end", ">=", issue.issue_date),
+        for claim in self:
+            claim.evaluation_id = evaluation_obj.search([
+                ("academic_year_id", "=", claim.academic_year_id.id),
+                ("center_id", "=", claim.school_id.id),
+                ("course_id", "=", claim.student_course_id.id),
+                ("date_start", "<=", claim.issue_date),
+                ("date_end", ">=", claim.issue_date),
             ], limit=1)
