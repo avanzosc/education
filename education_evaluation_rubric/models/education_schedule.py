80c929f52fc3bd351e4b345c463c97a7aee2a2f8
Leire <leiremartinez@avanzosc.es>
2023-06-02 14:17:07 +0200

    [IMP]education_evaluation_rubric: from schedules add shortcut to rubric questions

---
+++ education_evaluation_rubric/models/education_schedule.py
@@ -0,0 +1,31 @@
+# Copyright 2023 Leire Martinez de Santos - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo import _, api, models
+
+
+class EducationSchedule(models.Model):
+    _inherit = 'education.schedule'
+
+    @api.multi
+    def button_open_rubric_questions(self):
+        self.ensure_one()
+        domain = []
+        survey_ids = self.notebook_line_ids.mapped('edited_survey_id')
+        survey_question_ids = None
+        if not survey_ids:
+            n_line_survey_ids = self.notebook_line_ids.mapped('survey_id')
+            survey_ids = self.env['survey.survey'].search([
+                ('id', 'in', n_line_survey_ids.ids),
+            ])
+        if survey_ids:
+            survey_question_ids = self.env['survey.question'].search([
+                ('survey_id', 'in', survey_ids.ids),
+            ])
+            domain += [('id', 'in', survey_question_ids.ids)]
+        return {
+            'type': 'ir.actions.act_window',
+            'name': _('Schedule rubric questions'),
+            'res_model': 'survey.question',
+            'view_mode': 'tree',
+            'domain': domain,
+        }
