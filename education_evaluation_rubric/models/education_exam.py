6694cc92367c174de855a03dd6533ed8eb80a36d
Oihane Crucelaegui <oihanecruce@gmail.com>
2023-09-27 12:25:52 +0200

    [IMP] education_evaluation_rubric: rubric only in exams

--- education_evaluation_rubric/models/education_exam.py
+++ education_evaluation_rubric/models/education_exam.py
@@ -1,34 +1,52 @@
 # Copyright 2023 Leire Martinez de Santos - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import api, fields, models
-from odoo import exceptions
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class EducationExam(models.Model):
     _inherit = 'education.exam'
 
-    survey_id = fields.Many2one('survey.survey', string='Survey Template')
-    edited_survey_id = fields.Many2one('survey.survey', string='Custom Survey Template')
-    edited_survey_show = fields.Boolean(default=False, string='Custom Survey Show')
+    survey_id = fields.Many2one(comodel_name='survey.survey', string='Survey Template')
+    edited_survey_id = fields.Many2one(
+        comodel_name='survey.survey', string="Custom Survey Template")
+    edited_survey_show = fields.Boolean(default=False, string='Show Custom Survey')
     is_rubric = fields.Boolean(default=False, string='Is Rubric Exam')
-    survey_input_ids = fields.Many2many(
-        string="Survey Inputs",
-        comodel_name="survey.user_input",
-        relation="survey_input_exam_rel",
-        column1="exam_id",
-        column2="input_id",
-        compute="_compute_survey_input")
-    survey_input_count = fields.Integer('Survey input count', compute="_compute_survey_input_count")
+    # survey_input_ids = fields.Many2many(
+    #     string="Survey Responses",
+    #     comodel_name="survey.user_input",
+    #     relation="survey_input_exam_rel",
+    #     column1="exam_id",
+    #     column2="input_id",
+    #     compute="_compute_survey_input")
+    survey_input_ids = fields.One2many(
+        comodel_name="survey.user_input", inverse_name="exam_id",
+        string="Survey Responses")
+    survey_input_count = fields.Integer(
+        string="# Survey Responses", compute="_compute_survey_input")
+    missing_survey_inputs = fields.Boolean(
+        string="Missing Survey Responses", compute="_compute_missing_survey_inputs"
+    )
 
     def _compute_survey_input(self):
         for record in self:
-            record.survey_input_ids = self.env['survey.user_input'].search([
-                ('exam_id', '=', record.id)
-            ])
-
-    def _compute_survey_input_count(self):
-        for record in self:
             record.survey_input_count = len(record.survey_input_ids)
+
+    def _compute_missing_survey_inputs(self):
+        for record in self:
+            missing_inputs = False
+            if (record.is_rubric and
+                not all([bool(r.survey_input_id) for r in record.record_ids])):
+                    missing_inputs = True
+            record.missing_survey_inputs = missing_inputs
+
+    def button_create_missing_survey_inputs(self):
+        for record in self:
+            if record.is_rubric and (record.edited_survey_id or record.survey_id):
+                missing_inputs = record.record_ids.filtered(
+                    lambda r: not r.survey_input_id)
+                missing_inputs.create_survey_input(
+                    record.edited_survey_id or record.survey_id)
 
     @api.multi
     def action_survey_user_input(self):
@@ -43,9 +61,9 @@ class EducationExam(models.Model):
     @api.multi
     def action_generate_record(self):
         for record in self.filtered(lambda e: e.state in ('draft', 'progress')):
-            if record.is_rubric and not record.survey_id:
-                raise exceptions.ValidationError('Please Select Survey for Rubric Competence')
-
+            if record.is_rubric and not (record.survey_id or record.edited_survey_id):
+                raise ValidationError(
+                    _('Please Select Survey for Rubric Competence'))
         super(EducationExam, self).action_generate_record()
 
     @api.multi
@@ -54,16 +72,17 @@ class EducationExam(models.Model):
             if record.survey_id:
                 if record.survey_input_ids:
                     record.survey_input_ids.unlink()
-                record.edited_survey_id = record.survey_id.copy({
-                    'responsible': record.teacher_id.id,
-                    'create_uid': self.env.uid,
+                record.edited_survey_id = record.survey_id.copy(default={
+                    "responsible": record.teacher_id.id,
+                    "create_uid": self.env.uid,
+                    "title": "{} ({})".format(record.survey_id.title,
+                                              record.teacher_id.display_name)
                 })
                 # for page in record.sudo().survey_id.page_ids:
                 #     page = page.copy({
                 #         'survey_id': record.edited_survey_id.id,
                 #     })
-                record.edited_survey_id.title = record.survey_id.title + '(' + record.teacher_id.display_name + ')'
-                record.record_ids.create_survey_input()
+                record.record_ids.create_survey_input(record.edited_survey_id)
                 record.edited_survey_show = True
                 record.edited_survey_id.copy_survey_texts(record.survey_id.sudo())
 
@@ -76,68 +95,15 @@ class EducationExam(models.Model):
     @api.multi
     def button_open_all_survey_inputs(self):
         self.ensure_one()
-        first_survey = self.env['survey.user_input'].search([
-            ('id', 'in', self.survey_input_ids.ids)], order="id asc")
-        res = first_survey[0].button_respond_survey()
-        res['target'] = 'new'
+        first_survey = self.env["survey.user_input"].search([
+            ("id", "in", self.survey_input_ids.ids),
+            ("state", "!=", "done")
+        ], order="id asc")
+        res = first_survey[:1].button_respond_survey()
+        res["target"] = 'new'
         return res
 
     def get_survey_url(self):
-        for record in self:
-            if record.survey_id and record.competence_id.is_rubric:
-                url_get = record.button_open_all_survey_inputs()
-                return url_get.get('url', None)
-
-#
-# class EducationExam(models.Model):
-#     _inherit = 'education.exam'
-#
-#     survey_input_ids = fields.Many2many(
-#         string="Survey Inputs",
-#         comodel_name="survey.user_input",
-#         relation="survey_input_exam_rel",
-#         column1="exam_id",
-#         column2="input_id",
-#         compute="_compute_survey_input"
-#     )
-#     survey_input_count = fields.Integer('Survey input count', compute="_compute_survey_input_count")
-#     survey_id = fields.Many2one('survey.survey', compute='compute_survey')
-#
-#     def compute_survey(self):
-#         for record in self:
-#             record.survey_id = record.n_line_id.edited_survey_id.id if record.n_line_id.edited_survey_id else record.n_line_id.survey_id.id
-#
-#     def _compute_survey_input(self):
-#         for record in self:
-#             record.survey_input_ids = self.env['survey.user_input'].search([
-#                 ('exam_id', '=', record.id)
-#             ])
-#
-#     def _compute_survey_input_count(self):
-#         for record in self:
-#             record.survey_input_count = len(record.survey_input_ids)
-#
-#     @api.multi
-#     def button_open_all_survey_inputs(self):
-#         self.ensure_one()
-#         first_survey = self.env['survey.user_input'].search([
-#             ('id', 'in', self.survey_input_ids.ids)], order="id asc")
-#         res = first_survey[0].button_respond_survey()
-#         res['target'] = 'new'
-#         return res
-#
-#     @api.multi
-#     def action_survey_user_input(self):
-#         self.ensure_one()
-#         action_rec = self.env.ref('survey.action_survey_user_input')
-#         action = action_rec.read()[0]
-#         action.update({
-#             'domain': [('exam_id', '=', self.id)],
-#         })
-#         return action
-#
-#     def get_survey_url(self):
-#         for record in self:
-#             if record.survey_id and record.survey_input_ids:
-#                 url_get = record.button_open_all_survey_inputs()
-#                 return url_get.get('url', None)
+        self.ensure_one()
+        url_get = self.button_open_all_survey_inputs()
+        return url_get.get("url", None)
--- education_evaluation_rubric/models/education_record.py
+++ education_evaluation_rubric/models/education_record.py
@@ -1,30 +1,41 @@
 # Copyright 2021 Leire Martinez de Santos - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import api, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 
 class EducationRecord(models.Model):
     _inherit = 'education.record'
 
-    survey_input_id = fields.Many2one('survey.user_input', string='Survey Input')
-    survey_id = fields.Many2one('survey.survey', compute='compute_survey')
+    survey_input_id = fields.Many2one(
+        comodel_name='survey.user_input', string='Survey Input')
+    survey_input_state = fields.Selection(
+        string="Survey Response State", related="survey_input_id.state"
+    )
+    survey_id = fields.Many2one(
+        comodel_name='survey.survey', compute='_compute_survey')
     quizz_score = fields.Float(related='survey_input_id.quizz_score')
 
-    def compute_survey(self):
+    @api.depends("exam_id", "exam_id.edited_survey_id", "exam_id.survey_id")
+    def _compute_survey(self):
         for record in self:
-            record.survey_id = record.n_line_id.edited_survey_id.id if record.n_line_id.edited_survey_id else record.n_line_id.survey_id.id
+            record.survey_id = (record.exam_id and
+                                (record.exam_id.exam.edited_survey_id or
+                                 record.exam_id.survey_id))
 
-    def create_survey_input(self):
+    def create_survey_input(self, survey):
+        user_input_obj = self.env['survey.user_input']
+        if not survey:
+            return
         for record in self:
-            if record.survey_id:
-                record.survey_input_id = self.env['survey.user_input'].create({
-                    'survey_id': record.survey_id.id,
-                    'partner_id': record.student_id.id,
-                    'responsible': record.teacher_id.id,
-                    'notebook_line_id': record.n_line_id.id,
-                    'exam_id': record.exam_id.id if record.exam_id else None,
-                    'education_record_id': record.id,
-                })
+            record.survey_input_id = user_input_obj.create({
+                'survey_id': survey.id,
+                'partner_id': record.student_id.id,
+                'responsible': record.teacher_id.id,
+                'notebook_line_id': record.n_line_id.id,
+                'exam_id': record.exam_id.id if record.exam_id else None,
+                'education_record_id': record.id,
+            })
 
     def button_respond_survey(self):
         self.ensure_one()
@@ -34,7 +45,9 @@ class EducationRecord(models.Model):
 
     def create(self, vals):
         res = super(EducationRecord, self).create(vals)
-        res.create_survey_input()
+        if vals.get("exam_id"):
+            exam = self.env["education.exam"].browse(vals.get("exam_id"))
+            res.create_survey_input(exam.edited_survey_id or exam.survey_id)
         return res
 
     def set_numeric_mark(self, mark):
@@ -52,13 +65,13 @@ class EducationRecord(models.Model):
     def compute_numeric_mark(self):
         self.ensure_one()
         ret_value = 0.0
-        if self.survey_id.related_record_mark == 'quizz_score':
+        survey = self.survey_input_id.survey_id
+        if survey.related_record_mark == 'quizz_score':
             ret_value = self.quizz_score
-        elif self.survey_id.related_record_mark == 'average_grade':
+        elif survey.related_record_mark == 'average_grade':
             ret_value = self.survey_input_id.average_grade
-        elif self.survey_id.related_record_mark == 'maximum_average':
+        elif survey.related_record_mark == 'maximum_average':
             self.survey_input_id.update_line_value_suggested()
-            survey = self.survey_input_id.survey_id
             max_value = survey.mapped('page_ids').mapped('question_ids').mapped('labels_ids').sorted(
                 key=lambda m: m.quizz_mark, reverse=True)[0]
             max_points = max_value.quizz_mark * len(survey.mapped('page_ids').mapped('question_ids').mapped('labels_ids_2'))
@@ -69,7 +82,9 @@ class EducationRecord(models.Model):
         return ret_value
 
     def get_survey_url(self):
-        for record in self:
-            if record.survey_id and record.survey_input_id:
-                url_get = record.button_respond_survey()
-                return url_get.get('url', None)
+        self.ensure_one()
+        if not self.survey_input_id:
+            return UserError(_("No survey user input to show."))
+        url_get = self.button_respond_survey()
+        return url_get.get('url', None)
+
