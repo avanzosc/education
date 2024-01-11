dd0772e1469f2693bac93c3220088dcfa8972682
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-01-11 12:24:03 +0100

    [FIX] education_evaluation_rubric: singleton error without mapped function

--- education_evaluation_rubric/models/res_partner.py
+++ education_evaluation_rubric/models/res_partner.py
@@ -14,5 +14,5 @@ class ResPartner(models.Model):
         academic_records = self.get_academic_records_non_curricular(
             academic_year=academic_year, eval_type=eval_type
         )
-        return academic_records.child_record_ids.filtered(
+        return academic_records.mapped("child_record_ids").filtered(
             lambda r: r.n_line_id.competence_id.eval_mode == "rubric")
